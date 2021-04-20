361f7b209b69fe99fca75f86a8c457195a3e2416
tbeleyur <tbeleyur@orn.mpg.de>
2021-04-20 15:45:01 +0200

    add notebook with full-scale calibrations and the module to convert between dbu and vp2p

--- mic-calibration/fullscale_calculations.py
+++ mic-calibration/fullscale_calculations.py
@@ -15,20 +15,30 @@ dbu_ref = 0.775 # volts
 def dbu2vrms(dbu_value):
     return dbu_ref*10**(dbu_value/20)
 
+def vrms2dbu(vrms_value):
+    return 20*np.log10(vrms_value/dbu_ref)
+
 def vrms2vp2p(vrms_value):
     return 2*np.sqrt(2)*vrms_value
+
+def vpp2rms(vpp_value):
+    return vpp_value/(2*np.sqrt(2))
 
 def dbu2vp2p(dbu_value):
     vrms = dbu2vrms(dbu_value)
     vp2p = vrms2vp2p(vrms)
     return vp2p
 
+def vpp2dbu(vpp_value):
+    vrms = vpp2rms(vpp_value)
+    return vrms2dbu(vrms)
+    
 
 #%%
 # The max input levels for the Fireface 802 is different across the 
 # front and back channels, also depending on if it's a mic or instrument
 # input. 
 
-max_in_level = np.array([10,19,21])
+#max_in_level = np.array([10,19,21])
 
-print(dbu2vp2p(max_in_level))
+#print(dbu2vp2p(max_in_level))
