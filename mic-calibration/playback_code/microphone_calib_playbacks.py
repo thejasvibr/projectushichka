ba6c5c3169e8efd0381f545510e70ce20ca98eb7
tbeleyur <tbeleyur@orn.mpg.de>
2021-05-28 17:46:48 +0200

    add ampXspeaker notebook which finally sorts out the problem of speaker playback inconsistency

--- mic-calibration/playback_code/microphone_calib_playbacks.py
+++ mic-calibration/playback_code/microphone_calib_playbacks.py
@@ -214,9 +214,9 @@ if __name__ == '__main__':
 # WAV.write('pbk_v2.wav', int(fs),all_audio)
     
     playback_sounds, numsamples_comlength = make_v2_playback_sounds()
-    mic_num = 'gras-2mwall-flush'
+    mic_num = 'gras-2mwall_amp401_speaker550'
     angle = '0'
-    gain = '70'
+    gain = '50'
     orientation='azimuth'
     kwargs = {'save_path':'../'}
     #all_pbk_sounds = np.concatenate(pbk_sounds)
