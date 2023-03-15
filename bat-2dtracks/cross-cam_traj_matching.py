b1cea40796119ef8254babb6ceabca91a0bb287d
tbeleyur <tbeleyur@orn.mpg.de>
2023-03-15 18:04:44 +0100

    Add koger_tracking to VC

--- bat-2dtracks/cross-cam_traj_matching.py
+++ bat-2dtracks/cross-cam_traj_matching.py
@@ -8,6 +8,7 @@ Attempts at manually matching trajectories from different cameras
 """
 import pandas as pd 
 import glob
+import numpy as np 
 import matplotlib
 import trackpy as tp
 #%% Load the 2D trajectories from each of the cameras
@@ -18,6 +19,25 @@ cam_linked_data = {camid : pd.read_csv(linked) for camid, linked in zip(['K1','K
 # Perform required manual corrections and then assign each trajectory a unique
 # ID
 
+def correct_trajectory_labels(linked_trajs, traj_labels, action):
+    corrected = linked_trajs.copy()
+    if action == 'delete':
+        for each in traj_labels:
+            assert len(each)==1
+            linked_trajs = corrected[~corrected['particle']==each]
+    if action == 'fuse':
+        for each in traj_labels:
+            assert len(each)==2
+            relevant_rows = np.logical_or(corrected['particle']==each[0],
+                                          corrected['particle']==each[1])
+            corrected.loc[relevant_rows, 'particle']
+            
+
+# For K3, delete trajectories 8,7,11,16,0,5 and fuse {3,10}, {1,12}
+
+k3_corrected = cam_linked_data['K3'].copy()
+k3_traj_delete = [7,8,11,16,0,5]
+k3_fuse = [(3,10), (1,12)]
 
 #%% 
 # Try to match the trajectories with each other by drawing the epipolar lines
