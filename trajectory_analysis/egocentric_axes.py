2fce2fa2066cf53c0e6f9590cb910138524e75df
tbeleyur <tbeleyur@orn.mpg.de>
2023-12-13 18:43:36 +0100

    Build up on proximity_heatmap.py - generate simple animations & initial analyses looking into distances of cave in  egocentric axes across time

--- trajectory_analysis/egocentric_axes.py
+++ trajectory_analysis/egocentric_axes.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Calculating egocentric coordinate systems
-=========================================
+Calculating egocentric coordinate systems and ray-tracing
+=========================================================
 
 
 
@@ -9,9 +9,10 @@ Created on Sun Dec 10 23:38:14 2023
 
 @author: theja
 """
-
+from itertools import product
 import numpy as np 
-from scipy.spatial import transform
+from scipy.spatial import transform, distance_matrix
+
 
 def get_azimuth(xyz):
     x,y,z = xyz
@@ -34,47 +35,6 @@ def get_xyz(r,theta,phi):
     y = r*np.sin(theta)*np.sin(phi)
     z = r*np.cos(theta)
     return np.array([x,y,z])
-
-
-def calculate_egocentric_xyz(flight_direction, roll, ):
-    '''
-    
-    Parameters
-    ----------
-    flight_direction : (3,) np.array
-        Unit vector with the flight direction from t_N-1 to t_N
-    
-    roll : float
-        Roll in radians. Here 0 roll is when the vector points to the 
-        north pole. 
-
-    Returns
-    -------
-    egocentric_xyz : (3,3) np.array
-        The rotated xyz coordinates, centred on the origin itself.
-        Each row is one axis' vector.
-    
-    Note
-    ----
-    Translation to the actual trajectory needs to be done separately. 
-    
-    '''
-    # This is azimuth and elevation of the flight-aligned y-axis
-    azimuth, elevation = get_azimuth(flight_direction), get_elevation(flight_direction)
-    
-    # now calculate the azimuth of the x-axis
-    azimuth_x = azimuth - np.pi/2
-    # and the roll is the elevation. (top is 0)
-    elevation_x = roll
-    calculated_xaxis = get_xyz(1, elevation_x, azimuth_x, )
-    # The z-axis is the upward pointing vector that is perpendicular to 
-    # the x and y.
-    calculated_zaxis = np.cross(calculated_xaxis, flight_direction)
-    egocentric_xyz = np.row_stack((calculated_xaxis, 
-                                   flight_direction, 
-                                   calculated_zaxis))
-    return egocentric_xyz
-
 
 def calculate_2egocentric_xyz(flight_direction, roll):
     '''
@@ -106,14 +66,84 @@ def calculate_2egocentric_xyz(flight_direction, roll):
 
     return with_roll_xyz
 
-
-# #%%
-# if __name__ == "__main__":
-#     pp = pv.Plotter()
+def make_rays(phi_vals=np.linspace(0,360,9),
+                      theta_vals=np.linspace(0,180,11)):
+    '''
+    Makes rays of unit length emanating from the global origin 0,0,0
     
-#     pp.add_mesh(pv.Arrow(direction=x_the_perpendicular), color='r')
-#     pp.add_mesh(pv.Arrow(direction=flight_direction), color='g')
-#     pp.add_mesh(pv.Arrow(direction=z_the_perpendicular), color='b')
-#     pp.show()
+    TODO
+    ----
+    * Detect redundant theta-phi combinations?
+
+    Parameters
+    ----------
+    phi_vals : (N,) np.array 
+        The azimuth angles in degrees. Defaults to 5 rays spread out around 360 deg. 
+    theta_vals : (M,) np.array
+        The elevation angles in degrees. Defaults to 10 rays spread out around 180 deg. 
+
+    Returns 
+    -------
+    unique_phi_theta : (N,2) np.array
+        Azimuth and elevation values in the 0th and 1st column respectively.
+    unique_rays : (N,3) np.array
+        Unit vectors that correspond to the rays centred on the origin.
+    
+    '''
+    theta_phi_combis = list(product(phi_vals.tolist(), theta_vals.tolist()))
+   
+    all_ray_vectos = []
+    for (phi, theta) in theta_phi_combis:
+        xyz = get_xyz(1, np.radians(theta), np.radians(phi))
+        all_ray_vectos.append(xyz)
+    
+    all_rays = np.array(all_ray_vectos)
+    all_rays[np.abs(all_rays)<1e-15] = 0
+    # simplify as all_rays has repeats 
+    unique_rays, row_inds = np.unique(all_rays, axis=0, return_index=True)
+    unique_phi_theta = [theta_phi_combis[each] for each in row_inds]
+    return unique_phi_theta, unique_rays
 
 
+
+def find_ray_intersection(cave, xyz, ray_vector, amp_factor=50):
+    '''
+    Finds the closest point to the ray_vector. If none is found then 
+    a np.nan is returned. 
+
+    Parameters
+    ----------
+    cave : pv.PolyData
+        Mesh
+    xyz : (3,) np.array
+    ray_vector: (3,) np.array
+        Unit vector with the ray direction 
+    amp_factor : float>1
+        Amount by which to multiply the ray_vector
+    
+    Returns 
+    -------
+    points :np.array
+        Points in the cave that coincide with the exteneded ray
+    '''
+    extended_ray = xyz + ray_vector*amp_factor
+    points, ind = cave.ray_trace(xyz, extended_ray)
+    if points.size>3:
+        all_dists = distance_matrix(xyz.reshape(1,3), points)
+        closest_point_ind = np.argmin(all_dists)
+        points = points[closest_point_ind,:]
+    elif points.size==0:
+        points = np.tile(np.nan,3)
+    return points, extended_ray
+
+def get_all_ray_intersections(rays, *args):
+    cave, xyz, amp_factor = args
+    all_intersection_points = np.zeros(rays.shape)
+    all_extended_rays = np.zeros(rays.shape)
+    for row in range(rays.shape[0]):
+        point, extended_ray = find_ray_intersection(cave, xyz, rays[row,:])
+        all_intersection_points[row,:] = point
+        all_extended_rays[row,:] = extended_ray
+    all_intersection_points = np.array(all_intersection_points).reshape(-1,3)
+    return all_intersection_points, all_extended_rays
+    
--- trajectory_analysis/proximity_heatmap_etc.py
+++ trajectory_analysis/proximity_heatmap_etc.py
@@ -5,6 +5,32 @@ Proximity profiles
 This analysis will explore and try to quantify various biologically relevant
 movement parameters that we could extract from the 
 
+
+TODO
+~~~~
+* Use a larger ROI and close-up the cave mesh - some rays don't ever hit the cave mesh because of the gaps in the
+ceiling/floor
+
+* Some more work needs to be done in dealing with nans in the interpolation/extrapolation
+of xyz, velocities and accelerations.
+
+DONE
+~~~~
+* Find a more rigorous way to model the roll in the flight trajectories. UPDATE:
+  I realised what I need is not actual body roll, but really the head roll - as
+  this is the 'sensor' here. Anecdotal evidence from Ofri Eitan, Arjan Boonman,
+  Lasse Jakobsen and Felix Haefele suggest that bats keep their heads level - at
+  least in gentle turns. In sharp turns the head likely follows along the body a bit. 
+anecdotal evidence actually suggests that bats DONT roll their heads as they turn (see
+email exchanges in ab.mpg.de account).
+* TICK. Clean up the specks and free-floating points in the cave mesh - it affects the
+ray-tracing.
+
+Thoughts
+~~~~~~~~
+* Can do ray-tracing. And now what? 
+* 
+
 Created on Tue Sep 12 19:36:41 2023
 
 @author: theja
@@ -16,16 +42,16 @@ import numpy as np
 import os
 import pandas as pd
 import pyvista as pv
-from scipy.spatial import distance, transform
+from scipy.spatial import distance, transform, distance_matrix
 from scipy.interpolate import interp1d
 import sys 
 sys.path.append('..\\bat-video-tracking\\')
 from tracking_correction_utils import interpolate_xyz, calc_speed
-
+import tqdm
 #%%
 datafolder = os.path.join('..',
                           'thermo_lidar_alignment')
-mesh_path = os.path.join(datafolder, 'data','lidar_roi.ply')
+mesh_path = os.path.join(datafolder, 'data','lidar_roi_nofreefloaters.ply')
 mesh = pv.read(mesh_path)
 
 
@@ -68,7 +94,6 @@ tmc1000_rotated.loc[:,'frame':].to_csv('2018-08-17_P01_1000TMC_200frames_nicecoo
 #%%
 target_dataset = tmc7000_xyz
 
-plot2 = pv.Plotter()
 
 def keyboard_callback():
     print(plot2.camera.position)
@@ -77,21 +102,22 @@ def keyboard_callback():
 
 
     
-plot2.add_mesh(mesh, opacity=0.3)
-plot2.camera_position = 'xz' # (0, -10, 0) #(-4.0658486275620795, 0.8678076888611709, 25) #(3.75, -2.05, -0.57)
-plot2.camera.position = (0,-5,0)
-# plot2.camera.azimuth = 0
-# plot2.camera.roll = 90
-# plot2.camera.elevation = 0 #-15
+# plot2 = pv.Plotter()
+# plot2.add_mesh(mesh, opacity=0.3)
+# plot2.camera_position = 'xz' # (0, -10, 0) #(-4.0658486275620795, 0.8678076888611709, 25) #(3.75, -2.05, -0.57)
+# plot2.camera.position = (0,-5,0)
+# # plot2.camera.azimuth = 0
+# # plot2.camera.roll = 90
+# # plot2.camera.elevation = 0 #-15
 
-plot2.camera.view_angle = 60
-plot2.add_points(points , color='r', render_points_as_spheres=True, point_size=10)
+# plot2.camera.view_angle = 60
+# plot2.add_points(points , color='r', render_points_as_spheres=True, point_size=10)
 
-axis_arrows = [pv.Arrow(np.zeros(3), each) for each in [[1,0,0], [0,1,0], [0,0,1]]]
-for c ,each in zip(['r','g','b'],axis_arrows):
-    plot2.add_mesh(each, color=c)
-plot2.add_key_event('c', keyboard_callback)
-plot2.show()
+# axis_arrows = [pv.Arrow(np.zeros(3), each) for each in [[1,0,0], [0,1,0], [0,0,1]]]
+# for c ,each in zip(['r','g','b'],axis_arrows):
+#     plot2.add_mesh(each, color=c)
+# plot2.add_key_event('c', keyboard_callback)
+# plot2.show()
 
 #%%
 trajnums = tmc1000['id'].unique()
@@ -130,38 +156,65 @@ outup  = tmc1000.groupby('id').apply(xyz_minmax)
 ids = tmc1000_rotated['id'].unique()
 rot_byid = tmc1000_rotated.groupby('id')
 
-traj_index = 1
+traj_index = 5
 
 thistraj = rot_byid.get_group(ids[traj_index])
 thistraj_points = thistraj.loc[:,'x':'z'].to_numpy()
-rows, _ = thistraj_points.shape
-thistraj_dirvectors = np.zeros((rows-1,3))
-
-for i in range(1,rows):
-    delta_xyz = thistraj_points[i,:]  - thistraj_points[i-1,:] 
-    thistraj_dirvectors[i-1,:] = delta_xyz/np.linalg.norm(delta_xyz)
-# Set the [1,0,0] as the 'x' vector and get the corresponding orthogonal Z vector
-
 
 
 #%%
 # Calculate accelaration: 
 # This formula is modified from Khandelwal & Hedrick 2021/2?
 # TODO - implement a spline version.
+from scipy.interpolate import interp1d, UnivariateSpline,InterpolatedUnivariateSpline
+from scipy.interpolate import make_smoothing_spline
 
 fps = 25
 frame_durn = 1/fps
-v_xyz = np.apply_along_axis(np.diff, 0, thistraj_points)/frame_durn
-a_xyz = np.apply_along_axis(np.diff, 0, v_xyz)/frame_durn
-ax, ay, az = [abs(a_xyz[:,i]) for i in range(3)]
-# coarsely calculate the roll angle:
-factor = 10 # IMPORTANT---THIS IS JUST SOME KIND OF SHORT-TERM FIXXXXXXX
-roll_angle = np.arctan2(ax+ay, az + factor*9.81)
-roll_angle_degrees = np.degrees(roll_angle)
 
+xyz_interpfun = {}
+
+v_xyz_interpfun = {}
+a_xyz_interpfun = {}
+
+no_na_rows = np.sum(~np.isnan(thistraj_points), 1)>0
+nona_thistrajpoints = thistraj_points[no_na_rows,:]
+nona_t = np.arange(thistraj_points.shape[0])[no_na_rows]
+
+xyz_interp = np.zeros(nona_thistrajpoints.shape)
+v_xyz_interp = np.zeros(nona_thistrajpoints.shape)
+a_xyz_interp = np.zeros(nona_thistrajpoints.shape)
+
+for col, axis in enumerate(['x','y','z']):
+    
+    xyz_interpfun[axis] = make_smoothing_spline(nona_t,
+                                          nona_thistrajpoints[:,col])
+    xyz_interp[:,col] = xyz_interpfun[axis](nona_t)
+    v_xyz_interpfun[axis] = xyz_interpfun[axis].derivative()
+    v_xyz_interp[:,col] = v_xyz_interpfun[axis](nona_t)/frame_durn
+    
+    a_xyz_interpfun[axis] = v_xyz_interpfun[axis].derivative()
+    a_xyz_interp[:,col] = a_xyz_interpfun[axis](nona_t)/frame_durn**2
+
+rows, _ = v_xyz_interp.shape
+thistraj_dirvectors = np.zeros((rows-1,3))
+
+for i in range(1,rows):
+    delta_xyz = xyz_interp[i,:]  - xyz_interp[i-1,:] 
+    thistraj_dirvectors[i-1,:] = delta_xyz/np.linalg.norm(delta_xyz)
+
+#%%
+colnum = 2
+axes= ['x','y','z']
 plt.figure()
-plt.plot(roll_angle_degrees)
+
+plt.plot(xyz_interp[:,colnum], label=axes[colnum] + 'interpolated')
+plt.plot(thistraj_points[:,colnum], label='raw ' + axes[colnum])
+plt.plot(v_xyz_interp[:,colnum], label='interp v')
+plt.plot(a_xyz_interp[:,colnum], label='interp a')
+
 plt.legend()
+
 #%%
 # Try out ay/(az+9.81) - Khandelwal & Tyson Roy Soc. 
 # Here the assumption is that the lizard was flying in the x direction
@@ -171,94 +224,97 @@ plt.legend()
 # to calculate accelaration 
 # 
 
+
+ax, ay, az = [a_xyz_interp[:,i] for i in range(3)]
+# coarsely calculate the roll angle:
+factor = 5 # IMPORTANT---THIS IS JUST SOME KIND OF SHORT-TERM FIXXXXXXX
+roll_angle = np.arctan2(ax+ay, az + factor*9.81)
+roll_angle_degrees = np.degrees(roll_angle)
+
+# plt.figure()
+# plt.plot(roll_angle_degrees)
+# plt.legend()
+
+#%%
 from egocentric_axes import *
 
-i = 30
+global_axis=[pv.Arrow([0,0,0], np.eye(3)[row,:], tip_radius=0.05, scale=0.25) for row in range(3) ]        
+phi_theta , ray_points = make_rays(np.linspace(0,360,5), np.linspace(0,180,3))
 
-# plot3 = pv.Plotter()
-    
-# plot3.add_mesh(mesh, opacity=0.3)
-# plot3.camera_position = 'xz' # (0, -10, 0) #(-4.0658486275620795, 0.8678076888611709, 25) #(3.75, -2.05, -0.57)
-# plot3.camera.position = (0,-5,0)
-
-# plot3.camera.view_angle = 60
-# plot3.add_points(thistraj_points , color='r', render_points_as_spheres=True, point_size=10)
-
-# axis_arrows = [pv.Arrow(np.zeros(3), each) for each in [[1,0,0], [0,1,0], [0,0,1]]]
-# for c ,each in zip(['r','g','b'],axis_arrows):
-#     plot3.add_mesh(each, color=c)
-
-# # now generate the ego-centric axes considering flight direction and roll
-ego_xyz = calculate_2egocentric_xyz(thistraj_dirvectors[i-1,:], -roll_angle[i-2])
-
-# ego_arrows = [pv.Arrow(thistraj_points[i,:], ego_xyz[j,:]) for j in range(3)]
-# ego_on_origin = [pv.Arrow( [0,0,0], ego_xyz[j,:], shaft_radius=0.01, tip_radius=0.05) for j in range(3)]
-
-# for c ,each, every in zip(['r','g','b'], ego_arrows, ego_on_origin):
-#     plot3.add_mesh(each, color=c)
-#     plot3.add_mesh(every, color=c, )
-#     #plot3.add_mesh(each2, color=c, )
-
-# plot3.show()
-
-#
-# What I actually need to do is to run a line from the ego-centric coordinates 
-# at various azimuth and elevation angles - and to then calculate the distance
-# between the intersection and the centre of the bat.
-rotation_mat, ssd = transform.Rotation.align_vectors(ego_xyz, np.eye(3))
-
-#
-import tqdm
-sp0  = pv.Sphere(radius=0.15, center=thistraj_points[i,:],
-                direction = ego_xyz[2,:], 
-                theta_resolution=12, 
-                phi_resolution=6)
-
-all_intersection_points = []
-all_intersection_rays = []
-
-for xyz in tqdm.tqdm(sp.points):    
-    extended_endpoint =  thistraj_points[i,:] + (xyz-thistraj_points[i,:])*100
-    points, ind = mesh.ray_trace(thistraj_points[i,:], extended_endpoint)
-    intersection = pv.PolyData(points)
-    all_intersection_points.append(intersection)
-    ray = pv.Line(thistraj_points[i,:],
-                  extended_endpoint)
-    all_intersection_rays.append(ray)
-
-ego_arrows = [pv.Arrow(thistraj_points[i,:], ego_xyz[j,:]) for j in range(3)]
+movie_mode = True
 
 plot4 = pv.Plotter()
+if movie_mode:
+    plot4.open_movie(f'{ids[traj_index]}_egocentric.mp4', framerate=fps, quality=7)
 plot4.add_mesh(mesh, opacity=0.3)
-plot4.add_points(thistraj_points , color='r', render_points_as_spheres=True, point_size=10)
-
-for c ,each, every in zip(['r','g','b'],axis_arrows, ego_arrows):
-    plot4.add_mesh(each, color=c)
-    plot4.add_mesh(every, color=c)
-
-# ray1 = pv.Line(thistraj_points[i,:], (sp.points[0,:]-thistraj_points[i,:])*10)
-
-for each_int, each_ray in zip(all_intersection_points, all_intersection_rays):
-    if each_int.points.size>0:
-        plot4.add_mesh(each_int,
-                        color="maroon", point_size=25
-                        , label="Intersection Points")
-    plot4.add_mesh(each_ray, line_width=2, color='k')
-
-#plot4.add_mesh(ray1, line_width=1, color='g')
-
-plot4.add_mesh(sp)
+plot4.add_points(xyz_interp , color='r', render_points_as_spheres=True, point_size=10)
 plot4.camera_position = 'xz' # (0, -10, 0) #(-4.0658486275620795, 0.8678076888611709, 25) #(3.75, -2.05, -0.57)
 plot4.camera.position = (0,-5,0)
 plot4.camera.view_angle = 60
-plot4.show()
 
 
-# from scipy.spatial import KDTree
-# # the direction of the sphere is wrong for now - but yes...
-# sensory_range = pv.Sphere(radius=0.15, direction=ego_xyz[2,:], 
-#                           center=thistraj_points[i,:], theta_resolution=12, 
-#                           phi_resolution=15)
-# tree = KDTree(mesh.points)
-# d_kdtree, idx = tree.query(sensory_range.points)
-# sensory_range["distances"] = d_kdtree
+
+all_bat2cave = []
+#for i in tqdm.trange(thistraj_dirvectors.shape[0]):
+for i in np.arange(0,thistraj_dirvectors.shape[0]):
+    # # now generate the ego-centric axes considering flight direction and roll
+    ego_xyz = calculate_2egocentric_xyz(thistraj_dirvectors[i,:], 0)
+    #
+    # What I actually need to do is to run a line from the ego-centric coordinates 
+    # at various azimuth and elevation angles - and to then calculate the distance
+    # between the intersection and the centre of the bat.
+    rotation_mat, ssd = transform.Rotation.align_vectors(ego_xyz, np.eye(3))
+    
+    # throw out rays at the required angular density - centred around the origin
+    bat_to_cave_distances = pd.DataFrame(columns=['frame','dist_to_surf'], index=range(ray_points.shape[0]))
+    bat_to_cave_distances['frame'] = i
+    bat_to_cave_distances['ray_index'] = range(ray_points.shape[0])
+    # Rotate the rays to match the rotation of the egocentric system and centre them
+    # on the animal
+    transformed_raypoints = rotation_mat.apply(ray_points) 
+    intersection_points, extended_rays = get_all_ray_intersections(transformed_raypoints,
+                                                                   mesh, xyz_interp[i,:],
+                                                                   10)
+    bat_to_cave_distances['dist_to_surf'] = distance_matrix(xyz_interp[i,:].reshape(1,3), 
+                                                            intersection_points).flatten()
+    all_bat2cave.append(bat_to_cave_distances)
+    intersection_actor = plot4.add_points(intersection_points, color='maroon', render_points_as_spheres=True,
+                     point_size=30)
+
+    rays = [pv.Line(xyz_interp[i,:], extended_rays[each,:]) for each in range(extended_rays.shape[0])]
+    ray_actors = []
+    for cc, ray in zip(['r', 'g', 'b','c', 'm','y'], rays):
+        ray_actors.append(plot4.add_mesh(ray, color=cc))
+    #plot4.remove_scalar_bar()
+    ego_arrows = [pv.Arrow(xyz_interp[i,:], ego_xyz[row,:], scale=0.5) for row in range(3)]
+    egoaxis_actors = []
+    for c ,each in zip(['r','g','b'], ego_arrows):
+        egoaxis_actors.append(plot4.add_mesh(each, color=c))
+    # plot4.show(auto_close=False)
+    if movie_mode:
+        plot4.write_frame() 
+    
+    #plot4.show(auto_close=True)  # only necessary for an off-screen movie
+
+    [plot4.remove_actor(each) for each in egoaxis_actors]
+    plot4.remove_actor(intersection_actor)
+    [plot4.remove_actor(each) for each in ray_actors]
+plot4.close()
+#%%
+
+bat2cave = pd.concat(all_bat2cave).reset_index(drop=True)
+intuitive_to_phitheta = {j : dirn for j, dirn in enumerate(['left', 'back', 'down', 'right', 'front', 'up'])}
+
+plt.figure()
+for (ray_idx, subdf) in bat2cave.groupby('ray_index'):
+    
+    plt.plot(subdf.sort_values('frame')['dist_to_surf'], label=intuitive_to_phitheta[ray_idx])
+plt.title("Traj index " + str(traj_index))
+plt.legend()
+
+    
+
+
+
+
+
